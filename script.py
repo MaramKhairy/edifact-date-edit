3523498849148a216780893217f48daa2f6f0c9c
MaramKhairy <37209997+MaramKhairy@users.noreply.github.com>
2020-01-15 16:00:42 +0100

    finalized script
    Used to modify .edi files in a provided directory by updating dates in the INV, FAL and DAT segments.

--- script.py
+++ script.py
@@ -2,19 +2,20 @@ import re
 import string
 import os, time
 import datetime
-import fileinput
+import os
+import keyword
 
 #regexINV = r"^(?:INV)\+(?:[^+\n]*\+){4}(\d{8})"
 #regexFal = r"^(?:FAL)\+(?:[^+\n]*\+){6}(\d{8})"
 #regexDat = r"(?<=DAT\+)\d{8}"
 
+path = input("Folder path:  " )
+path = path + "/"
 
 invDays = int(input("INV days: " ))
 falDays = int(input("FAL days: "))
 datDays = int(input("DAT days: "))
 
-#formatedINVdate = ("\\g<1>" + INVDays)
-#print(formatedINVdate)
 
 def getSegmentName(line): 
     return line.split("+")[0]
@@ -37,31 +38,40 @@ def subDate(line,days,indexNr):
     print("joined splitted line: ", joinSplittedLine)
     return joinSplittedLine
 
+       
+directory = os.listdir(path)
+
+for file in directory:
+    print("file name:" , file) 
+    with open (path + file , "r+", encoding='utf-8-sig') as edifactile:
+        segments = edifactile.readlines()
+        versionNumber = getVersionNumber(segments)
+        segmentsNew = []
+        for segment in segments:
+            segmmentName = getSegmentName(segment)
+            if segmmentName == "INV":
+                segmentsNew.append(subDate(segment, invDays, 5))       
+            elif segmmentName == "FAL":
+                if (versionNumber == "1" or "2"):
+                    segmentsNew.append(subDate(segment, invDays, 7)) 
+                elif versionNumber > "2":
+                    segmentsNew.append(subDate(segment, invDays, 6)) 
+            elif segmmentName == "DAT": 
+                segmentsNew.append(subDate(segment, invDays, 1)) 
+            else:
+                segmentsNew.append(segment)
+        edifactile.seek(0)
+        edifactile.truncate()
+        edifactile.writelines(segmentsNew)
+
+os.system("pause")
 
 
-segmentsNew = []
-global segments  
-with open ("testedifact.edi" , "r+") as edifactile:
-    segments = edifactile.readlines()
-    versionNumber = getVersionNumber(segments)
-    for segment in segments:
-        segmmentName = getSegmentName(segment)
-        if segmmentName == "INV":
-            segmentsNew.append(subDate(segment, invDays, 5))       
-        elif segmmentName == "FAL":
-            if (versionNumber == "1" or "2"):
-                segmentsNew.append(subDate(segment, invDays, 7)) 
-            elif versionNumber > "2":
-                segmentsNew.append(subDate(segment, invDays, 6)) 
-        elif segmmentName == "DAT": 
-            segmentsNew.append(subDate(segment, invDays, 1)) 
-        else:
-            segmentsNew.append(segment)
-edifactile.close()
-with open ("testedifact.edi" , "w") as edifactile:
-    print(segmentsNew)
-    edifactile.writelines(segmentsNew)
-edifactile.close()
+
+
+
+
+
 
 
         
